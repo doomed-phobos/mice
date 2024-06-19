44cd407ae6ae3cd37a124186b86947870f36ff89
ediazva <ediazva@unsa.edu.pe>
2024-06-18 19:42:48 -0500

    Improving namespace li

--- li.cc
+++ li.cc
@@ -1,181 +1,187 @@
 #include "li.h"
 
-#include <cstring>
+// #include <cstring>
 #include <fcntl.h>
+#include <iostream>
 #include <unistd.h>
 #include <poll.h>
-#include <linux/input.h>
+#include <errno.h>
 #include <libinput.h>
-#include <libudev.h>
+// #include <libudev.h>
 
-#define SEATNAME "seat0"
-#define safe_call(ev, args) do {if(ev) ev(args);} while(0)
+#define SEATNAME "seat0" // TODO: Cambiar esto?
 
 namespace li {
-   const libinput_interface& LibInterface::GetInterface() {
-      static libinput_interface interface {
-         .open_restricted = open_restricted,
-         .close_restricted = close_restricted,
-      };
+  const libinput_interface& LibInterface::GetInterface() {
+    static libinput_interface interface {
+      .open_restricted = open_restricted,
+      .close_restricted = close_restricted,
+    };
 
-      return interface;
-   }
+    return interface;
+  }
+  
+  int LibInterface::open_restricted(const char* path, int flags, void* /*user_data*/) {
+    int fd = open(path, flags);
+    return fd < 0 ? -errno : fd;
+  }
+  
+  void LibInterface::close_restricted(int fd, void* /*user_data*/) {
+    close(fd);
+  }
 
-   int LibInterface::open_restricted(const char* path, int flags, void* /*user_data*/) {
-      int fd = open(path, flags);
-      return fd < 0 ? -errno : fd;
-   }
+  std::ostream& LibInput::log = std::cout;
 
-   void LibInterface::close_restricted(int fd, void* /*user_data*/) {
-      close(fd);
-   }
+  void LibInput::Deleter::operator()(libinput* ptr) const {
+    libinput_unref(ptr);
+  }
+  
+  void LibInput::startWaitEvents() {
+    pollfd fds {
+      .fd = libinput_get_fd(m_li.get()),
+      .events = POLLIN,
+      .revents = 0,
+    };
 
-   std::shared_ptr<LibInput> LibInput::MakeFromPaths(const std::vector<std::string>& paths) {
-      libinput* li;
+    m_shouldStop = false;
+    do {
+      nextEvent();
+    } while(!m_shouldStop && poll(&fds, 1, 5000) > 0);
+  }
 
-      li = libinput_path_create_context(&LibInterface::GetInterface(), nullptr);
-      if(!li) {
-         perror("Failed to create context");
-         return nullptr;
-      }
+  void LibInput::stopWaitEvents() {
+    m_shouldStop = true;
+  }
 
-      std::shared_ptr<LibInput> lib(new LibInput(li));
-      for(const auto& path : paths)
-         if(!add_device_from_path(li, path))
-            return nullptr;
+  void LibInput::nextEvent() {
+    libinput_dispatch(m_li.get());
+    for(libinput_event* ev; (ev = libinput_get_event(m_li.get())); libinput_event_destroy(ev)) {
+      processEvent(ev);
+    }
+  }
+  
+  void LibInput::processEvent(libinput_event* ev) {
+  }
 
-      return std::move(lib);
-   }
+  std::unique_ptr<LibInput> LibInput::MakeFromUDev() {
+    struct Udev {
+        udev* ptr;
+        
+        Udev() :
+          ptr{udev_new()} {}
 
-   std::shared_ptr<LibInput> LibInput::MakeFromUDev() {
-      libinput* li;
-      udev* uv = udev_new();
-      if(!uv) {
-         perror("Failed to initialize udev");
-         return nullptr;
-      }
+        ~Udev() {
+          udev_unref(ptr);
+        }
 
-      li = libinput_udev_create_context(&LibInterface::GetInterface(), nullptr, uv);
-      if(!li) {
-         perror("Failed to initialize libinput context from udev");
-         return nullptr;
-      }
+        operator udev*() {return ptr;}
+    } uv;
+    
+    if(!uv) {
+      log << "Failed to initialize udev";
+      return nullptr;
+    }
+    
+    Ptr li(libinput_udev_create_context(&LibInterface::GetInterface(), nullptr, uv));
+    if(!li) {
+      log << "Failed to initialize libinput context from udev";
+      return nullptr;
+    }
 
-      std::shared_ptr<LibInput> lib(new LibInput(li));
-      if(libinput_udev_assign_seat(li, SEATNAME)) {
-         perror("Failed to set seat");
-         udev_unref(uv);
-         return nullptr;
-      }
+    if(libinput_udev_assign_seat(li.get(), SEATNAME)) {
+      log << "Failed to set seat";
+      return nullptr;
+    }
 
-      udev_unref(uv);
-      return std::move(lib);
-   }
+    return std::unique_ptr<LibInput>{new LibInput(std::move(li))};
+  }
 
-   bool LibInput::add_device_from_path(libinput* li, const std::string& path) {
-      if(path.empty() || !li)
-         return false;
-      
-      libinput_device* device;
+#if 0
+  std::unique_ptr<LibInput> LibInput::MakeFromPaths(const std::vector<std::string>& paths) {
+    Ptr li(libinput_path_create_context(&LibInterface::GetInterface(), nullptr));
+    if(!li) {
+        log << "Failed to create context";
+        return nullptr;
+    }
 
-      device = libinput_path_add_device(li, path.c_str());
-      if(!device) {
-         return false;
-         fprintf(stderr, "Failed to open '%s': %s\n", path.c_str(), strerror(errno));
-      }
+    // std::unique_ptr<LibInput> lib(new LibInput(li));
+    for(const auto& path : paths)
+        if(!add_device_from_path(li, path))
+          return nullptr;
 
-      return true;
-   }
+    return lib;
+  }
 
-   LibInput::~LibInput() {
-      libinput_unref(m_li);
-      m_li = nullptr;
-   }
+  bool LibInput::add_device_from_path(Ptr& li, const std::string& path) {
+    libinput_device* device;
 
-   void LibInput::startWaitEvents() {
-      pollfd fds {
-         .fd = libinput_get_fd(m_li),
-         .events = POLLIN,
-         .revents = 0,
-      };
-      
-      m_stop = false;
-      do {
-         nextEvent();
-      } while(!m_stop && poll(&fds, 1, -1) > -1);
-   }
+    libinput_path_add_device(struct libinput *libinput, const char *path)
+    device = libinput_path_add_device(li, path.c_str());
+    if(!device) {
+        return false;
+        fprintf(stderr, "Failed to open '%s': %s\n", path.c_str(), strerror(errno));
+    }
 
-   void LibInput::stopWaitEvents() {
-      m_stop = true;
-   }
+    return true;
+  }
+#endif
+/*
+  void LibInput::handleEvents(libinput_event* ev) {
+    switch(libinput_event_get_type(ev)) {
+        case LIBINPUT_EVENT_DEVICE_ADDED:
+        case LIBINPUT_EVENT_DEVICE_REMOVED: {
+          libinput_device* dev = libinput_event_get_device(ev);
+          DeviceEvent d_ev;
+          bool hasCapability = false;
+          handleDefaultEvent(ev, d_ev);
 
-   void LibInput::nextEvent() {
-      libinput_event* ev;
+          if(libinput_device_has_capability(dev, LIBINPUT_DEVICE_CAP_KEYBOARD))
+              d_ev.type |= DeviceEvent::kKeyboard_Type, hasCapability = true;
+          if(libinput_device_has_capability(dev, LIBINPUT_DEVICE_CAP_POINTER))
+              d_ev.type |= DeviceEvent::kPointer_Type, hasCapability = true;
+          
+          if(!hasCapability)
+              d_ev.type = DeviceEvent::kUnknown_Type;
 
-      libinput_dispatch(m_li);
-      while((ev = libinput_get_event(m_li))) {
-         handleEvents(ev);
+          if(libinput_event_get_type(ev) == LIBINPUT_EVENT_DEVICE_ADDED)
+              safe_call(onDeviceAdded, d_ev);
+          else
+              safe_call(onDeviceRemoved, d_ev);
+        }
+        break;
+        case LIBINPUT_EVENT_POINTER_MOTION: {
+          auto p = libinput_event_get_pointer_event(ev);
+          PointerMotionEvent mev;
+          handleDefaultEvent(ev, mev);
+          
+          mev.x = static_cast<float>(libinput_event_pointer_get_dx(p));
+          mev.y = static_cast<float>(libinput_event_pointer_get_dy(p));
+          mev.ux = static_cast<float>(libinput_event_pointer_get_dx_unaccelerated(p));
+          mev.uy = static_cast<float>(libinput_event_pointer_get_dy_unaccelerated(p));
 
-         libinput_event_destroy(ev);
-      }
-   }
+          safe_call(onPointerMotion, mev);
+        }
+        break;
+        case LIBINPUT_EVENT_POINTER_BUTTON: {
+          auto p = libinput_event_get_pointer_event(ev);
+          PointerButtonEvent bev;
+          handleDefaultEvent(ev, bev);
 
-   void LibInput::handleEvents(libinput_event* ev) {
-      switch(libinput_event_get_type(ev)) {
-         case LIBINPUT_EVENT_DEVICE_ADDED:
-         case LIBINPUT_EVENT_DEVICE_REMOVED: {
-            libinput_device* dev = libinput_event_get_device(ev);
-            DeviceEvent d_ev;
-            bool hasCapability = false;
-            handleDefaultEvent(ev, d_ev);
+          uint32_t button = libinput_event_pointer_get_button(p);
+          bev.button = (button == BTN_LEFT ? PointerButtonEvent::kLeft_Button : 
+                        button == BTN_MIDDLE ? PointerButtonEvent::kMiddle_Button :
+                        button == BTN_RIGHT ? PointerButtonEvent::kRight_Button :
+                        PointerButtonEvent::kUnknown_Button);
+          bev.state = (libinput_event_pointer_get_button_state(p) == LIBINPUT_BUTTON_STATE_PRESSED ?
+                        PointerButtonEvent::kPressed_State : PointerButtonEvent::kReleased_State);
+          safe_call(onPointerButton, bev);    
+        }
+        break;
+    }   
+  }
 
-            if(libinput_device_has_capability(dev, LIBINPUT_DEVICE_CAP_KEYBOARD))
-               d_ev.type |= DeviceEvent::kKeyboard_Type, hasCapability = true;
-            if(libinput_device_has_capability(dev, LIBINPUT_DEVICE_CAP_POINTER))
-               d_ev.type |= DeviceEvent::kPointer_Type, hasCapability = true;
-            
-            if(!hasCapability)
-               d_ev.type = DeviceEvent::kUnknown_Type;
-
-            if(libinput_event_get_type(ev) == LIBINPUT_EVENT_DEVICE_ADDED)
-               safe_call(onDeviceAdded, d_ev);
-            else
-               safe_call(onDeviceRemoved, d_ev);
-         }
-         break;
-         case LIBINPUT_EVENT_POINTER_MOTION: {
-            auto p = libinput_event_get_pointer_event(ev);
-            PointerMotionEvent mev;
-            handleDefaultEvent(ev, mev);
-            
-            mev.x = static_cast<float>(libinput_event_pointer_get_dx(p));
-            mev.y = static_cast<float>(libinput_event_pointer_get_dy(p));
-            mev.ux = static_cast<float>(libinput_event_pointer_get_dx_unaccelerated(p));
-            mev.uy = static_cast<float>(libinput_event_pointer_get_dy_unaccelerated(p));
-
-            safe_call(onPointerMotion, mev);
-         }
-         break;
-         case LIBINPUT_EVENT_POINTER_BUTTON: {
-            auto p = libinput_event_get_pointer_event(ev);
-            PointerButtonEvent bev;
-            handleDefaultEvent(ev, bev);
-
-            uint32_t button = libinput_event_pointer_get_button(p);
-            bev.button = (button == BTN_LEFT ? PointerButtonEvent::kLeft_Button : 
-                          button == BTN_MIDDLE ? PointerButtonEvent::kMiddle_Button :
-                          button == BTN_RIGHT ? PointerButtonEvent::kRight_Button :
-                          PointerButtonEvent::kUnknown_Button);
-            bev.state = (libinput_event_pointer_get_button_state(p) == LIBINPUT_BUTTON_STATE_PRESSED ?
-                         PointerButtonEvent::kPressed_State : PointerButtonEvent::kReleased_State);
-            safe_call(onPointerButton, bev);    
-         }
-         break;
-      }   
-   }
-
-   void LibInput::handleDefaultEvent(libinput_event* li_ev, Event& ev) {
-      auto device = libinput_event_get_device(li_ev);
-      ev.sysname = libinput_device_get_sysname(device);
-   }
+  void LibInput::handleDefaultEvent(libinput_event* li_ev, Event& ev) {
+    auto device = libinput_event_get_device(li_ev);
+    ev.sysname = libinput_device_get_sysname(device);
+  }*/
 } // namespace li
--- li.h
+++ li.h
@@ -3,82 +3,91 @@
 // 
 #pragma once
 #include <memory>
-#include <vector>
-#include <functional>
+#include <iosfwd>
+// #include <vector>
+// #include <functional>
 
 struct libinput;
 struct libinput_event;
 struct libinput_interface;
 
 namespace li {
-   class LibInterface {
-   public:
-      static const libinput_interface& GetInterface();
-   private:
-      static int open_restricted(const char* path, int flags, void* user_data);
-      static void close_restricted(int fd, void* user_data);
-   };
+  class LibInterface {
+  public:
+    static const libinput_interface& GetInterface();
+  private:
+    static int open_restricted(const char* path, int flags, void* user_data);
+    static void close_restricted(int fd, void* user_data);
+  };
 
-   struct Event {
-      const char* sysname;
-   };
+  /*struct Event {
+    const char* sysname;
+  };
 
-   struct PointerMotionEvent : Event {
-      float x, y;   // Normal
-      float ux, uy; // Unaccelerated
-   };
+  struct PointerMotionEvent : Event {
+    float x, y;   // Normal
+    float ux, uy; // Unaccelerated
+  };
 
-   struct PointerButtonEvent : Event {
-      enum Button {
-         kNone_Button,
-         kLeft_Button,
-         kMiddle_Button,
-         kRight_Button,
-         kUnknown_Button,
-      } button = kNone_Button;
+  struct PointerButtonEvent : Event {
+    enum Button {
+        kNone_Button,
+        kLeft_Button,
+        kMiddle_Button,
+        kRight_Button,
+        kUnknown_Button,
+    } button = kNone_Button;
 
-      enum State {
-         kNone_State,
-         kPressed_State,
-         kReleased_State,
-      } state = kNone_State;
-   };
+    enum State {
+        kNone_State,
+        kPressed_State,
+        kReleased_State,
+    } state = kNone_State;
+  };
 
-   struct DeviceEvent : Event {
-      enum Type : int {
-         kUnknown_Type  = -1,
-         kNone_Type     = 0,
-         kPointer_Type  = 1 << 0,
-         kKeyboard_Type = 1 << 1,
-      };
+  struct DeviceEvent : Event {
+    enum Type : int {
+        kUnknown_Type  = -1,
+        kNone_Type     = 0,
+        kPointer_Type  = 1 << 0,
+        kKeyboard_Type = 1 << 1,
+    };
 
-      int type = kNone_Type;
-   };
+    int type = kNone_Type;
+  };*/
+  class LibInput {
+  public:
+    ~LibInput() {}
 
-   class LibInput {
-   public:
-      ~LibInput();
+    void startWaitEvents();
+    void stopWaitEvents();
+    // std::function<void(DeviceEvent)> onDeviceAdded;
+    // std::function<void(DeviceEvent)> onDeviceRemoved;
+    // std::function<void(PointerMotionEvent)> onPointerMotion;
+    // std::function<void(PointerButtonEvent)> onPointerButton;
 
-      void startWaitEvents();
-      void stopWaitEvents();
-      std::function<void(DeviceEvent)> onDeviceAdded;
-      std::function<void(DeviceEvent)> onDeviceRemoved;
-      std::function<void(PointerMotionEvent)> onPointerMotion;
-      std::function<void(PointerButtonEvent)> onPointerButton;
+    static std::unique_ptr<LibInput> MakeFromUDev();
+#if 0
+    static std::unique_ptr<LibInput> MakeFromPaths(const std::vector<std::string>& paths);
+#endif
+    static std::ostream& log;
+  private:
+    struct Deleter {
+      void operator()(libinput* ptr) const;
+    };
+    typedef std::unique_ptr<libinput, Deleter> Ptr;
+    LibInput(Ptr&& li) :
+      m_li{std::move(li)} {}
 
-      static std::shared_ptr<LibInput> MakeFromUDev();
-      static std::shared_ptr<LibInput> MakeFromPaths(const std::vector<std::string>& paths);
-   private:
-      LibInput(libinput* li) :
-         m_li{li} {}
+#if 0
+    static bool add_device_from_path(Ptr& li, const std::string& path);
+#endif
 
-      static bool add_device_from_path(libinput* li, const std::string& path);
+    void nextEvent();
+    void processEvent(libinput_event* ev);
+    // void handleDefaultEvent(libinput_event* li_ev, Event& ev);
 
-      void nextEvent();
-      void handleEvents(libinput_event* ev);
-      void handleDefaultEvent(libinput_event* li_ev, Event& ev);
-
-      libinput* m_li;
-      bool m_stop = false;
-   };
+    Ptr m_li;
+    bool m_shouldStop; // FIXME: Debería ser atómico?
+  };
 } // namespace li
